dc17b4246e6978cb5a427b0e30514c952d8f6bf7
krrish2025 <krrishdixit2006@gmail.com>
2025-11-21 01:21:05 +0530

    Final Assignment App GDG

--- AssignmentGDG/app/src/main/java/com/example/assignment_gdg/MainActivity3.kt
+++ AssignmentGDG/app/src/main/java/com/example/assignment_gdg/MainActivity3.kt
@@ -1,10 +1,12 @@
 package com.example.assignment_gdg
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,23 @@ class MainActivity3 : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val profile = findViewById<ImageView>(R.id.profile)
+
+        Glide.with(this)
+            .load(R.drawable.person_circle)
+            .into(profile)
+
+        val fb = findViewById<ImageView>(R.id.facebook)
+
+        Glide.with(this)
+            .load(R.drawable._023_facebook_icon)
+            .into(fb)
+
+        val tw = findViewById<ImageView>(R.id.twitter)
+
+        Glide.with(this)
+            .load(R.drawable.twitter_03)
+            .into(tw)
     }
 }
